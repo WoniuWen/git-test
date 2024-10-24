682c55307554745f33c35142ccd0e4034da7b12d
woniu <1583377175@qq.com>
2024-10-24 22:31:27 +0800

    master commit1

--- src/main/java/com/gittest/pojo/GitTest.java
+++ src/main/java/com/gittest/pojo/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git9");
         System.out.println("hello git10");
+        System.out.println("git1");
     }
 }

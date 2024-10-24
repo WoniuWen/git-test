952ba5287f59bb1c40c68b3950943e8736be7a82
woniu <1583377175@qq.com>
2024-10-24 22:27:49 +0800

    h first commit

--- src/main/java/com/gittest/pojo/GitTest.java
+++ src/main/java/com/gittest/pojo/GitTest.java
@@ -2,10 +2,7 @@ package com.gittest.pojo;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git4");
         System.out.println("hello git9");
-        System.out.println("hello git3");
         System.out.println("hello git10");
     }
 }

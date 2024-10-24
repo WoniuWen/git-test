c76d78c06e42831ba3fe7af67cc9af27071aced1
woniu <1583377175@qq.com>
2024-10-24 22:20:44 +0800

    third commit

--- src/main/java/com/gittest/pojo/GitTest.java
+++ src/main/java/com/gittest/pojo/GitTest.java
@@ -4,8 +4,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git4");
-        System.out.println("hello git");
+        System.out.println("hello git9");
         System.out.println("hello git3");
-        System.out.println("hello git");
+        System.out.println("hello git10");
     }
 }
